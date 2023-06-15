9f0a735751cbbc3612201ce85693c429bd94c662
Reza Vilera <vilera_reza99@yahoo.com>
2023-06-15 10:35:30 +0700

    Creating arguments

--- start_cam.py
+++ start_cam.py
@@ -1,85 +1,102 @@
-import asyncio
+'''
+[Last updated: 13-06-2023]
+This program will be running on Raspberry Pi Device.
+The program monitors for eye blink condition by locating the face landmarks -
+and measure the Eye Aspect Ratio (EAR). 
+The EAR below the specified threshold for specified period of time will be considered as "eyes closed' condition.
+The 'eyes closed' condition will drive a specified GPIO pin that can be connected to external device such as relay.
+'''
+import argparse
+import time
+from threading import Condition, Thread
 import cv2 as cv
+import dlib
 import numpy as np
 from scipy.spatial import distance as dist
-import dlib
-import time
-from threading import Condition
-#from asyncio import Condition
 
-# from skimage.color import rgb2gray
+from relay import Relay
 
-# Face predictor path
-predictor_path = 'shape_predictor_68_face_landmarks.dat'
 
-# define a video capture object
-vid = cv.VideoCapture(0)
-
-# dlib face detector
-face_detector = dlib.get_frontal_face_detector()
-# dlib face landmark predictor
-face_predictor = dlib.shape_predictor(predictor_path)
-
-# Right eye landmark index
-r_index = [*range(36, 42)]
-# left eye landmark index
-l_index = [*range(42, 48)]
-# Target size
-img_size = (640, 480)
-
-blink_condition = Condition()
+# Relay Pin
+relay = None
+# Blink and unblink condition variables
+blink_event_condition = None
+unblink_event_condition = None
+# Flag for blink
 is_blinked = False
+# Flag for presence of face
+is_face = False
 
 
-# async def time_the_blink(timeout = 3):
+# Waiting for the blink
+def wait_for_blink(timeout = 3):
 
-#     global blink_condition
-#     global is_blinked
-    
-#     with blink_condition:
-
-#         print ('wait the blink')
-#         if not (blink_condition.wait(timeout = timeout)):
-#             print ('Blink timeout')
-#             # Do something here
-#         else:
-#             print ('blink refreshed')
-        
-#         is_blinked = False
-
-
-async def wait_for_blink(timeout = 3):
-
-    print ('task2')
-    # global blink_condition
-    # global is_blinked
-    
-    # with blink_condition:
-
-    #     print ('wait the blink')
-    #     if not (blink_condition.wait(timeout = timeout)):
-    #         print ('Blink timeout')
-    #         # Do something here
-    #     else:
-    #         print ('blink refreshed')
-
-
-async def start_camera(blink_threshold = 0.2, blink_timeout = 3):
-
-    global blink_condition
+    global blink_event_condition
+    global unblink_event_condition
     global is_blinked
+    global is_face
+    global relay
+    
+    while True:
 
-    async def shape_to_np(shape):
+        # Delay to let the relay on after blink timeout. 
+        # Also meant to prevent intense loop when there is no face
+        time.sleep(1.5)
+
+        if not is_blinked:
+
+            # Wait for the blink event
+            with blink_event_condition:
+                print ('Wait for blink...')
+                blink_event_condition.wait()
+                # Restart the loop if there is no face detected
+                if not is_face:
+                    # Turn off the relay
+                    relay.off()
+                    continue
+                print ('Blinked...')
+
+        # Wait for unblink event
+        with unblink_event_condition:
+            print ('Waiting for unblink...')
+            if not (unblink_event_condition.wait(timeout=timeout)):
+                print ('Unblink timeout')
+                # DRIVE BLINK RELAY HERE
+                relay.on()
+            else:
+                # Restart the loop if there is no face detected
+                if not is_face:
+                    # Turn off the relay
+                    relay.off()
+                    continue
+                print ('blink refreshed')
+                # Turn off the relay
+                relay.off()
+
+
+def start_camera(blink_threshold = 0.25, 
+                 unblink_threshold = 0.28, 
+                 flip = True, 
+                 res=(640,480),
+                 predictor_path = '.'
+                 ):
+
+    global relay
+    global blink_event_condition
+    global unblink_event_condition
+    global is_blinked
+    global is_face
+
+    def shape_to_np(shape):
 
         coords = np.zeros((shape.num_parts, 2), dtype='int')
-
         for i in range (shape.num_parts):
             coords [i] = (shape.part(i).x, shape.part(i).y)
         
         return coords
 
 
-    async def calculate_ear(eye):
+    def calculate_ear(eye):
 
         # Distance of vertical landmarks 1
         a = dist.euclidean(eye[1], eye[5])
@@ -91,82 +108,150 @@ async def start_camera(blink_threshold = 0.2, blink_timeout = 3):
         # Eye aspect ratio
         return ((a+b) / (2*c))
 
+    # define a video capture object
+    vid = cv.VideoCapture(0)
+    # Setting resolution
+    #vid.set(3, 640)
+    #vid.set(3, 480)
+    # dlib face detector
+    face_detector = dlib.get_frontal_face_detector()
+    # dlib face landmark predictor
+    face_predictor = dlib.shape_predictor(predictor_path)
+    # blink and unblink condition variables
+    # Right eye landmark index
+    r_index = [*range(36, 42)]
+    # left eye landmark index
+    l_index = [*range(42, 48)]
+    # Make sure relay is off before starting
+    relay.off()
 
     while(True):
 
-        t1 = time.time()
-        ret, frame = vid.read()
-        # print (frame.shape)
-        # Frame conversion to gray
-        # Resize the frame
-        #h, w = frame.shape[:2]
-        # factor = img_size[0]/w
-        # frame = cv.resize(frame, (int(w*factor), int(h*factor)))
-        img_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        #img_gray = (rgb2gray(frame[:,:,::-1])*255).astype('uint8')
-        # Need to 
-        # Face detection
-        rects = face_detector(img_gray, 0)
-        # print (rect)
-        # Display the resulting frame
-        # cv.imshow('frame', frame)
+        try:
+            #t1 = time.time()
+            # Read the frame
+            ret, frame = vid.read()
+            # Frame conversion to gray
+            img_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+            # Flip
+            if flip:
+                img_gray = cv.rotate(img_gray, cv.ROTATE_180)
+            # Face detection
+            rects = face_detector(img_gray, 0)
 
-        for rect in rects:
-            shape = face_predictor(img_gray, rect)
-            shape = await shape_to_np(shape)
-            eye_l = shape[l_index]
-            eye_r = shape[r_index]
-            ear_r = await calculate_ear(eye_r)
-            ear_l = await calculate_ear(eye_l)
-            # ear_avg = (ear_r + ear_l) / 2
-            ear_max = max(ear_r, ear_l)
-            # print (f'ear: {ear_max}')
+            if len(rects) == 0:
+                # Face is detected
+                print ('no face')
+                # No face is detected
+                is_face = False
+                # Reset the blink flag
+                is_blinked = False
+                # Clear the waiting thread
+                with blink_event_condition:
+                    blink_event_condition.notify_all()
+                with unblink_event_condition:
+                    unblink_event_condition.notify_all()
+                continue
 
-            # Check the blink
-            if ear_max < blink_threshold:
-                #print (f'ear: {ear_max}')
-                #print ('blink occured')
-                #if not is_blinked:
-                print ('blinked')
-                    # is_blinked=True
-                    # time_the_blink(timeout=blink_timeout)
             else:
-                pass
-                #with blink_condition:
-                #   blink_condition.notify_all()
+                # Face is detected
+                print ('face detected')
+                is_face = True
+                for rect in rects:
+                    shape = face_predictor(img_gray, rect)
+                    shape = shape_to_np(shape)
+                    eye_l = shape[l_index]
+                    eye_r = shape[r_index]
+                    ear_r = calculate_ear(eye_r)
+                    ear_l = calculate_ear(eye_l)
 
-            leftEyeHull = cv.convexHull(eye_l)
-            rightEyeHull = cv.convexHull(eye_r)
-            cv.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-            cv.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+                    #ear_max = max(ear_r, ear_l)
+                    ear_max = (ear_r + ear_l)/2
+                    print (f'ear: {ear_max}')
 
-        # Display the resulting frame
-        cv.imshow('frame', frame)
+                    # Check the blink
+                    if ear_max < blink_threshold:
+                        print ('blink occur')
+                        # Blink occur
+                        is_blinked = True
+                        with blink_event_condition:
+                            blink_event_condition.notify_all()
 
-        # the 'q' button is set as the
-        if cv.waitKey(1) & 0xFF == ord('q'):
+                    elif ear_max >= unblink_threshold:
+                        # Unblink occur
+                        is_blinked = False
+                        with unblink_event_condition:
+                            unblink_event_condition.notify_all()
+        
+            #t2 = time.time()
+            #print (f'frame_time: {t2-t1}')
+
+        except Exception as e:
+            print (e)
+            # On error, release the cap object
+            vid.release()
+            break
+
+
+def main(blink_thres = 0.25, 
+         unblink_thres = 0.28, 
+         blink_timeout = 3.0, 
+         flip=True, 
+         relay_pin=17, 
+         predictor_path = '.'):
+
+    global relay 
+    global blink_event_condition 
+    global unblink_event_condition
+    
+    print ('Starting...')
+
+    # Default Relay Pin on GPIO 17 (Raspberry Header Pin 11)
+    relay = Relay(pin=relay_pin)
+    # Blink and unblink condition variables
+    blink_event_condition = Condition()
+    unblink_event_condition = Condition()
+    # Start camera
+    start_camera_t = Thread(target = start_camera, args=(blink_thres, unblink_thres, flip, predictor_path))
+    # Wait for blink
+    wait_for_blink_t = Thread(target = wait_for_blink, args=(blink_timeout,))
+    start_camera_t.daemon = True
+    start_camera_t.start()
+    wait_for_blink_t.daemon = True
+    wait_for_blink_t.start()
+    print ('Running...')
+
+    while True:
+        time.sleep(3)
+        # Check the thread status and break if one of it no longer active
+        if not (start_camera_t.is_alive() and wait_for_blink_t.is_alive()):
+            # DRIVE ERROR SIGNAL HERE
             break
     
-        t2 = time.time()
-        # print (f'frame_time: {t2-t1}')
-
-    # After the loop release the cap object
-    vid.release()
-    # Destroy all the windows
-    cv.destroyAllWindows()
-
-async def main():
-    global blink_condition
-    # Start camera
-    task_start_camera = asyncio.create_task(start_camera())
-    #print ('task1')
-    task_wait_for_blink = asyncio.create_task(wait_for_blink())
-    #print ('task2')
-    #await task_start_camera
-    await task_wait_for_blink
-    with blink_condition:
-        blink_condition.notify_all()
-
+    # Make sure relay is off before quit
+    relay.off()
     print ('end')
 
-asyncio.run (main())
+
+if __name__ == '__main__':
+
+    # Argument handler
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--blink_thres', type = float, default = 0.25, required = False)
+    parser.add_argument('--unblink_thres', type = float, default = 0.28, required = False)
+    parser.add_argument('--blink_timeout', type = float, default = 3.0, required = False)
+    parser.add_argument('--flip', type = bool, default = True, required = False)
+    parser.add_argument('--relay_pin', type = int, default = 17, required = False)
+    parser.add_argument('--pred_path', type = str, default = '../shape_predictor_68_face_landmarks.dat', required = False)
+
+    # Parsing
+    args = parser.parse_args()
+    blink_thres = args.blink_thres
+    unblink_thres = args.blink_thres
+    blink_timeout = args.blink_timeout
+    flip = args.flip
+    relay_pin = args.relay_pin
+    predictor_path = args.pred_path
+
+    # Run
+    main(blink_thres, unblink_thres, blink_timeout, flip, relay_pin, predictor_path)
